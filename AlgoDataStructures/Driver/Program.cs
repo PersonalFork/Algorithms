7a0a9266732604126b2d59e9df04e38d4a738b1a
Jashobanta Chakraborty <jashobantac@gmail.com>
2019-01-09 00:51:22 +0530

    RotateArray algo.

--- AlgoDataStructures/Driver/Program.cs
+++ AlgoDataStructures/Driver/Program.cs
@@ -1,15 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using DataStructures.Arrays;
+
+using System;
 
 namespace Driver
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string[] array = new string[] { "1", "2", "3", "4", "5", "6", "7" };
+            array.Print();
+
+            #region Rotate By N
+
+            int n = 2;
+            Console.WriteLine("\nRotating by " + n);
+            array.RotateByN(n, RotateApproach.TempStorage);
+            Console.Read();
+
+            #endregion
         }
     }
 }
